091ffbd482cc5a8c12234724b6ab557872afb2ae
Pedro Dalbem <pedrolucas18dalbem@gmail.com>
2025-03-16 20:59:46 +0000

    cadastro de CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,5 +2,39 @@
 
 int main() {
 
-    return 0;
+    //carta 1
+    // declara uma variavel para armazenar uma letra
+   char estado1;
+   // Código da carta (ex: "A01") -> 3 caracteres + 1 nulo '\0'
+   char codigo1 [4];
+   // Nome da cidade (espaço para até 49 caracteres + '\0')
+   char cidade1 [50];
+    // Número de habitantes
+   int pipulação;
+   // Área em km²
+   float area1;
+   // PIB da cidade
+   float pib1;
+   // Número de pontos turísticos
+   int pontos1;
+
+   // Carta 2
+char estado2;
+char codigo2[4];      
+char cidade2[50];     
+int populacao2;
+float area2;
+float pib2;
+int pontos2;
+
+printf("Vamos começar seu jogo de super trunfo?\n");
+printf("Para iniciarmos vamos fazer o cadastro de suas cartas\n");
+
+printf("carta 1");
+printf("Digite uma letra de A a h para o primeiro estado");
+scanf("%c", estado1);
+
+printf("Digite um codigo de 01 a 04 para a carta");
+scanf("%s", codigo1);
+
 }
